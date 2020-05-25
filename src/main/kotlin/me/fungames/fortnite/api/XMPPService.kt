fa50a59ef47a51b7151ddd783147e04114193fcf
PizzaCrust <pizzacrustcoder@gmail.com>
2020-05-25 21:39:46 +0700

    Fix friend event issues

--- src/main/kotlin/me/fungames/fortnite/api/XMPPService.kt
+++ src/main/kotlin/me/fungames/fortnite/api/XMPPService.kt
@@ -29,10 +29,11 @@ open class PartyEvent(@SerializedName("party_id") var partyId: String = "") {
 data class PartyMemberEvent(@SerializedName("account_id") var accountId: String = ""): PartyEvent()
 data class InviteChangeEvent(@SerializedName("invitee_id") var inviteeId: String = "")
 data class PingEvent(@SerializedName("pinger_id") var pingerId: String = "")
-data class FriendEvent(val to: String?,
-                       val from: String?,
-                       val reason: String?,
-                       val status: String?)
+data class FriendEvent(val to: String? = "",
+                       val from: String? = "",
+                       val reason: String? = "",
+                       val status: String? = "",
+                       val timestamp: String? = "")
 
 sealed class NotificationType<T: Any>(val clazz: KClass<T>) {
 
@@ -71,7 +72,7 @@ fun Message.bodyAsJson(): JsonObject = Gson().fromJson(this.body, JsonObject::cl
 
 class XMPPService(val api: FortniteApi): StanzaListener {
 
-    private val connection: AbstractXMPPConnection
+    internal val connection: AbstractXMPPConnection
     val chat: ChatManager
     val roster: Roster
 
